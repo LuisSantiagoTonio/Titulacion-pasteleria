07e1534f0fb40c84582ec4da91bc37d0c9ca104b
LuiSantiagoTonio <santiagol59776@gmail.com>
2026-07-19 22:39:56 -0600

    Conectar backend con MySQL de Railway

--- Titulacion-pasteleria/server/src/db.js
+++ Titulacion-pasteleria/server/src/db.js
@@ -1,14 +1,14 @@
+
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'pasteleria_dashboard',
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
